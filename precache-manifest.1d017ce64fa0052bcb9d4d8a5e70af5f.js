1526fb0a05ccf441992a2f132c38dba3b59173ce
Neciu Dan <dan@adoreme.com>
2020-04-13 13:25:11 +0300

    first commit

---
+++ precache-manifest.1d017ce64fa0052bcb9d4d8a5e70af5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0af4bc01256a15b4536bbabc79882f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0de77641e7d8e1e06d52",
+    "url": "/static/css/main.45090789.chunk.css"
+  },
+  {
+    "revision": "081e85f021985bc78e75",
+    "url": "/static/js/2.66337dff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66337dff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de77641e7d8e1e06d52",
+    "url": "/static/js/main.454ff98e.chunk.js"
+  },
+  {
+    "revision": "5d17550fd08afd59123e",
+    "url": "/static/js/runtime-main.6510470d.js"
+  }
+]);
